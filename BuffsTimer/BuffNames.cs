a1dbf441c6a9395b24f5fd9736bc9f3618ef8664
crusifixknight <equinox.salieri@gmail.com>
2026-07-18 22:16:33 +0300

    wip

---
+++ BuffsTimer/BuffNames.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BuffsTimer;
+
+public static class BuffNames
+{
+    public static Dictionary<string, string> Dict;
+
+    static BuffNames()
+    {
+        Dict = new Dictionary<string, string>
+        {
+            {"addJump", "Jump higher"},
+            {"addSpeed", "Running faster"},
+            {"addClimb", "Climb faster"},
+            {"addGripStrength", "Increased grip strength"},
+            {"pilled", "Decreased stamina consumption"},
+            {"addStrike", "Increased melee damage"},
+            {"addHammer", "Increased piton-hammering ability"},
+            {"gripResist", "Reduced grip loss"},
+            {"damageResist", "Increased damage resist"},
+            {"regenerateGripStrength", "Increased stamina regeneration"},
+            {"addClimbSpeed", "Climb faster"},
+            {"roided", "Climb faster (???)"},
+            
+            
+        };
+    }
+}
